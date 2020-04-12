d9e75f1709da6fb0709f76d4bc1fd6e81cc0ab92
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-04-11 18:14:25 -0700

    variable declaration and print statement

--- Compiler/parseTree.pl
+++ Compiler/parseTree.pl
@@ -1,1 +1,26 @@
 % Parse Tree Generator
+
+:- use_rendering(svgtree).
+
+% Three Data type till now
+data_type(t_int_data_type(int)) --> [int].
+data_type(t_float_data_type(float)) --> [float].
+data_type(t_string_data_type(string)) --> [string].
+
+% Check for valid string need to add under score in grammmer
+% Also need to redefine grammer for string num string
+identifier(t_identifier(X)) -->[X], {atom(X)}.
+
+declaration(t_declaration_identifier(X, Y)) --> data_type(X), identifier(Y).
+
+printv(t_print(X)) --> [X].
+
+statement(t_statement_declaration(X)) --> declaration(X).
+statement(t_statement_print(X)) --> [print], ['('] , printv(X), [')'].
+
+command(t_command(X, Y)) --> statement(X), command(Y).
+command(t_command()) --> [].
+
+block(t_block(X))-->[start],command(X),[end].
+
+program(t_program(X))-->block(X), [.].
