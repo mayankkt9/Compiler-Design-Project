1e4ef4a0ee091c0cccc2f15f25be85e4a083d537
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-04-20 20:10:06 -0700

    Updated requierments

--- python_main.py
+++ python_main.py
@@ -1,68 +1,67 @@
 #!/usr/bin/env python3
-from nltk.tokenize import sent_tokenize, word_tokenize 
+from nltk.tokenize import sent_tokenize, word_tokenize
 from pyswip import Prolog
-import sys
-import operator
 from functools import reduce
-from pprint import pprint
+
 
 def lexer_job():
-	str2 = open('inputsourcecode.rch', 'r').read()
-	str1 = word_tokenize(str2)
-	lex=""
-	lex+='['
-	for i in range(0,len(str1)):
-		ascii = reduce(lambda x, y: str(x)+str(y), map(ord,str1[i]))
-		# print(str1[i]+ " "+str(ascii))
-		if i>1 and str1[i]=='=' and str1[i-1]==':':
-			continue
-		if str1[i]==':' and str1[i+1]=='=':
-			lex+=str1[i]+str1[i+1]
-			i+=2
-		elif str1[i]=='(':
-			lex+="'('"
-		elif str1[i]==')':
-			lex+="')'"
-		elif ascii == "9696":
-			lex+="'\"'"
-		elif ascii == "3939":
-			lex+="'\"'"
-		elif str1[i]=='}':
-			lex+="'}'"
-		elif str1[i]=='{':
-			lex+="'{'"
-		else:
-			lex+=str1[i]
-		lex+=','
-	lex = lex[:-1]
-	lex+=']'
-	print("LEXER")
-	print(lex)
-	return lex
+    str2 = open('inputsourcecode.rch', 'r').read()
+    str1 = word_tokenize(str2)
+    lex = ""
+    lex += '['
+    for i in range(0, len(str1)):
+        ascii = reduce(lambda x, y: str(x)+str(y), map(ord, str1[i]))
+        # print(str1[i]+ " "+str(ascii))
+        if i > 1 and str1[i] == '=' and str1[i-1] == ':':
+            continue
+        if str1[i] == ':' and str1[i+1] == '=':
+            lex += str1[i]+str1[i+1]
+            i += 2
+        elif str1[i] == '(':
+            lex += "'('"
+        elif str1[i] == ')':
+            lex += "')'"
+        elif ascii == "9696":
+            lex += "'\"'"
+        elif ascii == "3939":
+            lex += "'\"'"
+        elif str1[i] == '}':
+            lex += "'}'"
+        elif str1[i] == '{':
+            lex += "'{'"
+        else:
+            lex += str1[i]
+        lex += ','
+    lex = lex[:-1]
+    lex += ']'
+    print("LEXER")
+    print(lex)
+    return lex
+
 
 def create_parse_tree(lex):
 
-	prolog.consult("semantics.pl")
-	
-	query = "program(P,{},[])."
-	
-	parsetree = prolog.query(query.format(lex))
-	
-	return next(parsetree)["P"]
-	
+    prolog.consult("semantics.pl")
+
+    query = "program(P,{},[])."
+
+    parsetree = prolog.query(query.format(lex))
+
+    return next(parsetree)["P"]
+
 
 def give_semantics(parse_tree):
 
-	query = "program_eval({},Z)"
-	
-	Env = prolog.query(query.format(parse_tree))
+    query = "program_eval({},Z)"
 
-	print(next(Env)["Z"])
+    Env = prolog.query(query.format(parse_tree))
+
+    print(next(Env)["Z"])
 
 
 def set_up_env():
-	import nltk
-	nltk.download('punkt')
+    import nltk
+    nltk.download('punkt')
 
 
 # set_up_env()
@@ -70,6 +69,4 @@ prolog = Prolog()
 lex = lexer_job()
 parse_tree = create_parse_tree(lex)
 give_semantics(parse_tree)
-
-#program(P, [begin, var, z, ; , var, x, ;, z, :=, x, end, .], []), write(P), program_eval(P, 2, 3, Z).
 
